103377504afcb51b519c0d353caa3cbafaceaa6b
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-17 19:49:04 +0300

    Cleanup

--- dist/options/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type IMG from "./img";
-export interface Options {
-    [key: string]: any;
-    path?: string;
-    img?: boolean | IMG;
-    logger?: number;
-}
-declare const _default: () => Options;
-export default _default;
