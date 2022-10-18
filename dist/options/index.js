112138cbf476da61f82dfa588baaa072c4668656
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-18 12:47:51 +0300

    Cleanup

--- dist/options/index.js
+++ dist/options/index.js
@@ -1,1 +1,2 @@
-var e=()=>({path:"./dist/",img:{avif:{chromaSubsampling:"4:4:4",effort:9},gif:{effort:10},heif:{chromaSubsampling:"4:4:4"},jpeg:{chromaSubsampling:"4:4:4",mozjpeg:!0,trellisQuantisation:!0,overshootDeringing:!0,optimiseScans:!0},png:{compressionLevel:9,palette:!0},raw:{},tiff:{compression:"lzw"},webp:{effort:6}},logger:2});export{e as default};
+var e=()=>({path:"./dist/",img:{avif:{chromaSubsampling:"4:4:4",effort:9},gif:{effort:10},heif:{chromaSubsampling:"4:4:4"},jpeg:{chromaSubsampling:"4:4:4",mozjpeg:!0,trellisQuantisation:!0,overshootDeringing:!0,optimiseScans:!0},png:{compressionLevel:9,palette:!0},raw:{},tiff:{compression:"lzw"},webp:{effort:6}},logger:2});export { e as default };
+
