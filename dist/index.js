ba3b47b8f85d1319533942ec314a116a972df0d2
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-25 01:09:58 +0300

    squash!

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,1 @@
-import{deepmerge as p}from"deepmerge-ts";import e from"./options/index.js";var s=(r={})=>{var o;const t=p(e(),r);return t.path=(o=t.path)!=null&&o.endsWith("/")?t.path:`${t.path}/`,{name:"astro-img",hooks:{}}};export{s as default};
+import{deepmerge as r}from"deepmerge-ts";import p from"./options/index.js";var i=(o={})=>{const t=r(p(),o);return t.path=t.path?.endsWith("/")?t.path:`${t.path}/`,{name:"astro-img",hooks:{}}};export{i as default};
